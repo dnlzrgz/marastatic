9ccf98865fe16d4bdddda96c615488c31cebcf56
daniel <contact@dnlzrgz.com>
2026-05-01 20:12:01 +0200

    feat: remove watch

--- marastatic.py
+++ marastatic.py
@@ -6,8 +6,6 @@
 #   "jinja2>=3.1.6",
 #   "markdown>=3.10.1",
 #   "python-frontmatter>=1.1.0",
-#   "rich>=14.3.1",
-#   "watchfiles>=1.1.1",
 # ]
 # ///
 
@@ -17,19 +15,32 @@ from collections import defaultdict
 from dataclasses import dataclass, field
 from datetime import datetime
 from pathlib import Path
-from shutil import copytree, ignore_patterns
-from time import perf_counter
 from typing import Any
 
 import frontmatter
 import markdown
 import tomllib
-from jinja2 import Environment as Jinja2Environment
-from jinja2 import FileSystemLoader
-from rich.console import Console
-from watchfiles import watch
+from jinja2 import Environment, FileSystemLoader
 
-console = Console()
+# Logging helpers
+G, Y, R, B, RES = "\033[32m", "\033[33m", "\033[31m", "\033[1m", "\033[0m"
+
+
+def info(msg):
+    print(f"{B}# {RES}{'INFO':<4} {msg}")
+
+
+def ok(msg):
+    print(f"{B}{G}+ {RES}{'OK':<4} {msg}")
+
+
+def warm(msg):
+    print(f"{B}{Y}! {RES}{'WARN':<4} {msg}")
+
+
+def err(msg):
+    print(f"{B}{R}x {RES}{'ERR':<4} {msg}")
+
 
 MARKDOWN_CONVERTER = markdown.Markdown(extensions=["fenced_code", "tables", "abbr"])
 
@@ -41,12 +52,17 @@ class Config:
     static_dir: Path
     templates_dir: Path
     content_dir: Path
+    archetypes_dir: Path | None
     build_dir: Path
 
     params: dict[str, Any] = field(default_factory=dict)
 
     def __post_init__(self):
-        for field_name in ["static_dir", "templates_dir", "content_dir"]:
+        for field_name in [
+            "static_dir",
+            "templates_dir",
+            "content_dir",
+        ]:
             path = getattr(self, field_name)
             if not path.exists() or not path.is_dir():
                 raise FileNotFoundError(
@@ -73,10 +89,6 @@ class Page:
         return MARKDOWN_CONVERTER.reset().convert(self.raw_content)
 
 
-def list_content(path: Path) -> list[Path]:
-    return [f.relative_to(path) for f in path.rglob("*.md")]
-
-
 def load_config(config_file: Path) -> Config:
     if not config_file.exists():
         raise FileNotFoundError(f"Config file {config_file} not found.")
@@ -85,57 +97,87 @@ def load_config(config_file: Path) -> Config:
         data = tomllib.load(f)
 
     site_data = data.get("site", {})
+    archetypes_dir = site_data.get("archetypes_dir")
     return Config(
         base_url=site_data["base_url"],
         static_dir=Path(site_data["static_dir"]),
         templates_dir=Path(site_data["templates_dir"]),
         content_dir=Path(site_data["content_dir"]),
+        archetypes_dir=Path(archetypes_dir) if archetypes_dir else None,
         build_dir=Path(site_data["build_dir"]),
         params=data.get("params", {}),
     )
+
+
+def create_content(
+    config: Config,
+    archetype: str,
+    destination: Path,
+    open: bool = False,
+) -> None:
+    if not config.archetypes_dir:
+        err("archetypes directory not defined.")
+        return
+
+    archetype_file = config.archetypes_dir / f"{archetype}.md"
+    if not archetype_file.exists():
+        raise FileNotFoundError(f"Archetype file {archetype_file} not found.")
+
+    target = config.content_dir / destination
+    if target.exists():
+        warm(f"'{target}' already exists.")
+        return
+
+    target.write_text(archetype_file.read_text())
+    ok(f"created new {archetype} at '{target}'!")
+
+    if open:
+        import os
+        import subprocess
+
+        editor = os.environ.get("EDITOR")
+        if not editor:
+            err("no editor found in $EDITOR environment variables.")
+            return
+        try:
+            info(f"opening '{target}'...")
+            subprocess.run([editor, str(target)])
+        except Exception as e:
+            err(f"could not open the editor: {e}")
 
 
 def copy_static_files(config: Config) -> None:
     content_dir = config.content_dir
     output_dir = config.build_dir
     static_dir = config.static_dir
-    copytree(
+    shutil.copytree(
         content_dir,
         output_dir,
-        ignore=ignore_patterns("*.md", "*.xml"),
+        ignore=shutil.ignore_patterns("*.md", "*.xml"),
         dirs_exist_ok=True,
     )
-    console.print(
-        f"[green bold]Ok[/]: Cloned non-markdown files to '{output_dir.name}'"
-    )
+    ok(f"cloned non-markdown files to '{output_dir.name}'")
 
-    copytree(
+    shutil.copytree(
         static_dir,
         output_dir.joinpath(static_dir.name),
         dirs_exist_ok=True,
     )
-    console.print(
-        f"[green bold]Ok[/]: Cloned static folder '{static_dir.name}' into '{output_dir.name}'"
-    )
-
-
-def get_url(base_url: str, rel_path: Path) -> str:
-    path_str = rel_path.with_suffix(".html").as_posix()
-    return f"{base_url.rstrip('/')}/{path_str}"
+    ok(f"cloned static folder '{static_dir.name}' into '{output_dir.name}'")
 
 
 def get_all_pages(config: Config) -> list[Page]:
+    base_url = config.base_url
     pages = []
-    for rel_path in list_content(config.content_dir):
-        abs_path = config.content_dir / rel_path
-
+    for abs_path in config.content_dir.rglob("*.md"):
+        rel_path = abs_path.relative_to(config.content_dir)
         source = frontmatter.load(abs_path)
-
+        url = f"{base_url.rstrip('/')}/{rel_path.with_suffix('.html').as_posix()}"
         page = Page(
             rel_path=rel_path,
             metadata=source.metadata,
             raw_content=source.content,
-            url=get_url(config.base_url, rel_path),
+            url=url,
             dest_path=config.build_dir / rel_path.with_suffix(".html"),
         )
 
@@ -146,8 +188,8 @@ def get_all_pages(config: Config) -> list[Page]:
 
 def prepare_jinja_env(
     config: Config, pages: list[Page]
-) -> tuple[Jinja2Environment, dict[str, list[Page]]]:
-    jinja_env = Jinja2Environment(loader=FileSystemLoader(config.templates_dir))
+) -> tuple[Environment, dict[str, list[Page]]]:
+    jinja_env = Environment(loader=FileSystemLoader(config.templates_dir))
 
     sections = defaultdict(list)
     for page in pages:
@@ -165,7 +207,7 @@ def prepare_jinja_env(
 
 
 def generate_rss_feeds(
-    config: Config, jinja_env: Jinja2Environment, sections: dict[str, list[Page]]
+    config: Config, jinja_env: Environment, sections: dict[str, list[Page]]
 ) -> None:
     for section_name, pages in sections.items():
         if section_name == "root":
@@ -180,32 +222,30 @@ def generate_rss_feeds(
                 encoding="utf-8",
             )
 
-            console.print(f"[green bold]Ok[/]: Created RSS feed for '{section_name}'")
+            ok(f"created RSS feed for '{section_name}'")
         except Exception as e:
-            console.print(
-                f"[yellow bold]Warn[/]: No rss feed template found for '{section_name}': {e}"
-            )
+            warm(f"no rss feed template found for '{section_name}': {e}")
 
 
-def generate_sitemap(config: Config, jinja_env: Jinja2Environment) -> None:
+def generate_sitemap(config: Config, jinja_env: Environment) -> None:
     try:
         sitemap = jinja_env.get_template("sitemap.xml").render()
         (config.build_dir / "sitemap.xml").write_text(sitemap, encoding="utf-8")
-        console.print("[green bold]Ok[/]: Created sitemap.xml")
+        ok("created sitemap.xml")
     except Exception as e:
-        console.print(f"[yellow bold]Warn[/]: No sitemap.xml template found: {e}")
+        warm(f"no sitemap.xml template found: {e}")
 
 
-def generate_robots(config: Config, jinja_env: Jinja2Environment) -> None:
+def generate_robots(config: Config, jinja_env: Environment) -> None:
     try:
         robots = jinja_env.get_template("robots.txt").render()
         (config.build_dir / "robots.txt").write_text(robots, encoding="utf-8")
-        console.print("[green bold]Ok[/]: Created robots.txt")
+        ok("created robots.txt")
     except Exception as e:
-        console.print(f"[yellow bold]Warn[/]: No robots.txt template found: {e}")
+        warm(f"no robots.txt template found: {e}")
 
 
-def generate_pages(jinja_env: Jinja2Environment, pages: list[Page]) -> None:
+def generate_pages(jinja_env: Environment, pages: list[Page]) -> None:
     for page in pages:
         template_names = [
             page.rel_path.with_suffix(".html").as_posix(),
@@ -215,84 +255,79 @@ def generate_pages(jinja_env: Jinja2Environment, pages: list[Page]) -> None:
 
         try:
             template = jinja_env.get_or_select_template(template_names)
-            ouput = template.render(page=page)
+            output = template.render(page=page)
 
             page.dest_path.parent.mkdir(parents=True, exist_ok=True)
-            page.dest_path.write_text(ouput, encoding="utf-8")
-            console.print(f"[bold green]Ok[/]: Rendered '{page.url}' successfully")
+            page.dest_path.write_text(output, encoding="utf-8")
+            ok(f"rendered '{page.url}' successfully")
         except Exception as e:
-            console.print(
-                f"[red bold]Err[/]: No template found for '{page.rel_path}': {e}"
-            )
+            err(f"no template found for '{page.rel_path}': {e}")
 
 
 def clean(build_dir: Path) -> None:
-    console.print(f"🧹 Cleaning '{build_dir.name}'...")
     shutil.rmtree(build_dir)
     build_dir.mkdir(parents=True, exist_ok=True)
-    console.print(f"🧹 Cleaned '{build_dir.name}'!")
+    info(f"cleaned '{build_dir.name}'!")
 
 
 def build(config: Config) -> None:
-    start_time = perf_counter()
+    info(f"building {config.base_url}")
 
-    console.print(f"✨ Building {config.base_url}")
-
-    console.print("🌱 Scanning content directory...")
     pages = get_all_pages(config)
-    console.print(f"[bold green]Ok[/]: {len(pages)} pages found!")
+    ok(f"{len(pages)} pages found!")
 
-    console.print("🧰 Preparing Jinja2 environment...")
     jinja_env, sections = prepare_jinja_env(config, pages)
-
-    console.print("🖨️ Rendering pages...")
     generate_pages(jinja_env, pages)
-
-    console.print("📡 Generating RSS feeds, Sitemap and robots.txt...")
     generate_rss_feeds(config, jinja_env, sections)
     generate_sitemap(config, jinja_env)
     generate_robots(config, jinja_env)
-
-    console.print("📦 Copying static files...")
     copy_static_files(config)
 
-    end_time = perf_counter()
-    duration = end_time - start_time
-    console.print(f"🚀 Build complete in {duration:.2f}s!")
-
-
-def watch_and_rebuild(config: Config) -> None:
-    console.print("📡 Watching for changes...")
-
-    paths = [config.content_dir, config.templates_dir, config.static_dir]
-    for _ in watch(*paths):
-        console.print("📡 Changes detected! Rebuilding...")
-        build(config)
+    info("build complete!")
 
 
 def main():
     parser = argparse.ArgumentParser(
         prog="marastatic",
-        description="Single-file static site generator.",
+        description="single-file static site generator.",
     )
     parser.add_argument("--config-file", type=Path, default="config.toml")
-    parser.add_argument("--watch", action="store_true")
-    parser.add_argument("--clean", action="store_true")
-    args = parser.parse_args()
 
-    try:
-        config = load_config(args.config_file)
-        if args.clean:
+    subparsers = parser.add_subparsers(
+        dest="command",
+        help="Available commands.",
+    )
+
+    build_parser = subparsers.add_parser("build", help="Build the site.")
+    build_parser.add_argument("-c", "--clean", action="store_true")
+
+    new_parser = subparsers.add_parser("new", help="Create new content.")
+    new_parser.add_argument("archetype", type=str, help="Archetype name.")
+    new_parser.add_argument("destination", type=Path, help="Relative path to content.")
+    new_parser.add_argument(
+        "-o",
+        "--open",
+        action="store_true",
+        help="Open the file in your default editor.",
+    )
+
+    args = parser.parse_args()
+    command = args.command or "build"
+    config = load_config(args.config_file)
+
+    if command == "new":
+        create_content(config, args.archetype, args.destination, args.open)
+    else:
+        if getattr(args, "clean", False):
             clean(config.build_dir)
 
         build(config)
-        if args.watch:
-            watch_and_rebuild(config)
-    except KeyboardInterrupt:
-        console.print("👋🏻 bye!")
-    except Exception as e:
-        console.print(f"[bold red]Err[/]: {e}")
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        info("\nbye!")
+    except Exception as e:
+        err(e)
